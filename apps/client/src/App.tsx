f0eac2411ebfe8df480cb72f706f745714575388
inhachoi <chlruddlf73@naver.com>
2025-05-21 15:36:39 +0900

    ✨ feat: 페이지 라우팅 세팅

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -4,13 +4,36 @@ const App = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <h1>Welcom To Choi Space</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center space-y-12">
+        <h1>Welcome To Choi Space</h1>
 
-      <article onClick={() => navigate("/experiences")}>Experiences</article>
-      <article onClick={() => navigate("/projects")}>Projects</article>
-      <article onClick={() => navigate("/studies")}>Study Logs</article>
-    </>
+        <div className=" text-red-600">TEST</div>
+
+        <div className="flex justify-center gap-8">
+          <article
+            className="w-40 h-40 bg-white border border-gray-300 rounded-xl shadow-lg flex items-center justify-center cursor-pointer hover:bg-gray-100 transition"
+            onClick={() => navigate("/experiences")}
+          >
+            Experiences
+          </article>
+
+          <article
+            className="w-100 h-100 bg-white border border-gray-300 rounded-xl shadow-lg flex items-center justify-center cursor-pointer hover:bg-gray-100 transition"
+            onClick={() => navigate("/projects")}
+          >
+            Projects
+          </article>
+
+          <article
+            className="w-40 h-40 bg-white border border-gray-300 rounded-xl shadow-lg flex items-center justify-center cursor-pointer hover:bg-gray-100 transition"
+            onClick={() => navigate("/studies")}
+          >
+            Study Logs
+          </article>
+        </div>
+      </div>
+    </div>
   );
 };
 
