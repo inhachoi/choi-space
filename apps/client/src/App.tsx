7ccefa057ea95f9c9685c146810aea2740519baf
inhachoi <chlruddlf73@naver.com>
2025-05-26 16:50:00 +0900

    🔨 refactor: 코드 분리

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Card2 from "./components/Card2.js";
+import Card from "./components/Card/Card.js";
 
 const App = () => {
   const navigate = useNavigate();
@@ -24,15 +24,15 @@ const App = () => {
       </header>
 
       <main className="flex gap-10">
-        <Card2
+        <Card
           onClick={goToExperiences}
           imageUrl="https://i.pinimg.com/736x/58/cf/3d/58cf3ddf2a95a0a4e88ffc1c5ec1c530.jpg"
         />
-        <Card2
+        <Card
           onClick={goToProjects}
           imageUrl="https://i.pinimg.com/736x/73/df/c1/73dfc104387f112c59a6b7efabbc738d.jpg"
         />
-        <Card2
+        <Card
           onClick={goToStudies}
           imageUrl="https://i.pinimg.com/736x/5f/6e/f0/5f6ef0bb33d7925328e49e6e32064dd0.jpg"
         />
--- apps/client/src/components/Card/Card.tsx
+++ apps/client/src/components/Card/Card.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from "react";
+import "./Card.css";
 
 type CardProps = {
   onClick: () => void;
   imageUrl: string;
 };
 
-const Card2 = ({ onClick, imageUrl }: CardProps) => {
+const Card = ({ onClick, imageUrl }: CardProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const overlayRef = useRef<HTMLDivElement>(null);
 
@@ -16,10 +17,10 @@ const Card2 = ({ onClick, imageUrl }: CardProps) => {
     if (!container || !overlay) return;
 
     const handleMouseMove = (e: MouseEvent) => {
-      let x = e.offsetX;
-      let y = e.offsetY;
-      let rotateY = (-1 / 5) * x + 20;
-      let rotateX = (4 / 30) * y - 20;
+      const x = e.offsetX;
+      const y = e.offsetY;
+      const rotateY = (-1 / 5) * x + 20;
+      const rotateX = (4 / 30) * y - 20;
 
       overlay.style.backgroundPosition = `${x / 5 + y / 5}%`;
       container.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
@@ -49,42 +50,8 @@ const Card2 = ({ onClick, imageUrl }: CardProps) => {
           style={{ backgroundImage: `url(${imageUrl})` }}
         ></div>
       </div>
-
-      <style>
-        {`
-          .container {
-            width: 220px;
-            height: 310px;
-            transition: all 0.1s;
-            position: relative;
-          }
-          .overlay {
-            position: absolute;
-            width: 220px;
-            height: 310px;
-            background: linear-gradient(
-              105deg,
-              transparent 40%,
-              rgba(48, 57, 53, 0.6) 45%,
-              rgba(34, 45, 40, 0.6) 45%,
-              transparent 54%
-            );
-            filter: brightness(1.1) opacity(0.8);
-            mix-blend-mode: color-dodge;
-            background-size: 150% 150%;
-            background-position: 100%;
-            transition: all 0.1s;
-          }
-          .card {
-            width: 220px;
-            height: 310px;
-            background-image: url(https://i.pinimg.com/736x/5f/6e/f0/5f6ef0bb33d7925328e49e6e32064dd0.jpg);
-            background-size: cover;
-          }
-        `}
-      </style>
     </>
   );
 };
 
-export default Card2;
+export default Card;
